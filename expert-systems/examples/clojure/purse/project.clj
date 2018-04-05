f7d2aa059bcd8fd534e433ca010c3c0a29969424
whole-systems-enterprises <whole.systems.enterprises@gmail.com>
2018-04-05 14:41:09 -0700

    Added a blank line.

--- expert-systems/examples/clojure/purse/project.clj
+++ expert-systems/examples/clojure/purse/project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]
   		[com.cerner/clara-rules "0.17.0"]
 ]
