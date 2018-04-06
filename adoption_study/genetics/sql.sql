19bc7291197bd74878b98217f4567965b01c1a1f
whole-systems-enterprises <whole.systems.enterprises@gmail.com>
2018-04-05 18:28:20 -0700

    May delete these later.

--- adoption_study/genetics/sql.sql
+++ adoption_study/genetics/sql.sql
@@ -1,8 +1,6 @@
 .headers on
 
-SELECT DISTINCT ga.geneId, ga.geneName, d.diseaseId, lower(d.diseaseName)
-FROM geneDiseaseNetwork gdn, geneAttributes ga, diseaseAttributes d
-WHERE
-ga.geneNID = gdn.geneNID AND
-d.diseaseNID = gdn.diseaseNID
-;
+SELECT ga.geneId, ga.geneName, d.diseaseId, lower(d.diseaseName) FROM geneDiseaseNetwork gdn, geneAttributes ga, diseaseAttributes d WHERE ga.geneNID = gdn.geneNID AND d.diseaseNID = gdn.diseaseNID;
+
+
+
